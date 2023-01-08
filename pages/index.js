09fbf3248432742e051aa54b5a90da1b6f2bf16c
Wojciech Kowal <w.kowal202@gmail.com>
2023-01-08 15:39:38 +0100

    Add new content to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
   return (
-    <>
-      <main>
-        <h1>Hello there</h1>
-      </main>
-    </>
+    <main className={styles.mainContent}>
+      <h1>Hello there!</h1>
+      <h2>My name is Wojciech Kowal</h2>
+    </main>
   );
 }
