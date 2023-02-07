e482632b9d0478e8e78e4991d0db3d7a5c449608
Wojciech Kowal <w.kowal202@gmail.com>
2023-02-07 08:05:14 +0100

    Add object with links

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import styles from '../styles/Home.module.css';
+import BusinessCards from './components/BusinessCards';
+
+const links = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/wojtazk',
+    icon: '/images/icons/github-mark-white.svg',
+  },
+];
 
 export default function Home() {
   return (
     <main className={styles.mainContent}>
       <h1>Hello there!</h1>
-      <h2>My name is Wojciech Kowal</h2>
+      <h2>I'm Wojciech</h2>
+
+      <BusinessCards links={links} />
     </main>
   );
 }
