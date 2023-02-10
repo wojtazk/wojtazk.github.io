3c6e062487385d45eec9acd018b902c664d4b66d
Wojciech Kowal <w.kowal202@gmail.com>
2023-02-10 16:20:49 +0100

    Add codecademy, twitter & instagram links & logos

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,36 @@ import BusinessCards from './components/BusinessCards';
 
 const links = [
   {
-    name: 'GitHub',
+    site: 'LinkedIn',
+    username: 'kowal-wojciech',
+    url: 'https://www.linkedin.com/in/kowal-wojciech/',
+    icon: '/images/icons/LI-In-Bug.png',
+  },
+
+  {
+    site: 'GitHub',
+    username: 'wojtazk',
     url: 'https://github.com/wojtazk',
     icon: '/images/icons/github-mark-white.svg',
+  },
+
+  {
+    site: 'Codecademy',
+    username: 'wojtazk',
+    url: 'https://www.codecademy.com/profiles/wojtazk',
+    icon: '/images/icons/Codecademy_icon.svg',
+  },
+  {
+    site: 'Twitter',
+    username: 'wojtaz_k',
+    url: 'https://twitter.com/wojtaz_k',
+    icon: '/images/icons/twitter_logo_blue.svg',
+  },
+  {
+    site: 'Instagram',
+    username: 'kowalwojtek',
+    url: 'https://www.instagram.com/kowalwojtek/',
+    icon: '/images/icons/Instagram_Glyph_Gradient_RGB.svg',
   },
 ];
 
