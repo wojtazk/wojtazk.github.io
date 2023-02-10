e061f37d9f326b64f96b895ce13f9d4e012cfecf
Wojciech Kowal <w.kowal202@gmail.com>
2023-02-10 17:17:49 +0100

    Bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,7 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="description" content="Wojciech Kowal's Business Card" />
         <meta name="author" content="Wojciech Kowal" />
 
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {/* theme color */}
         <meta
